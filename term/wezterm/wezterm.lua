7dd5684a767225675ec2b5b446d045885e59344f
Francesco La Camera <fm@lacamera.org>
2025-06-27 02:59:42 +0200

    feat(wezterm): do not use window_decorations and shrink tab names

--- term/wezterm/wezterm.lua
+++ term/wezterm/wezterm.lua
@@ -1,5 +1,20 @@
 local wezterm = require("wezterm")
 
+wezterm.on('format-tab-title', function(tab)
+  local pane = tab.active_pane
+  local proc = pane.foreground_process_name or ''
+  local cmd = proc:gsub('.*[\\/]', '')
+
+  if cmd == '' then
+    cmd = pane.title or 'sh'
+  end
+
+  local label = string.format('%d: %s',
+    tab.tab_index, cmd)
+
+  return {{ Text = ' ' .. label .. ' ' }}
+end)
+
 local config = {}
 if wezterm.config_builder then
   config = wezterm.config_builder()
@@ -7,13 +22,16 @@ end
 
 config = {
   font_size = 16.0,
-  font = wezterm.font_with_fallback({
-    "JetBrains Mono"
+  font = wezterm.font({
+    family = "JetBrains Mono",
+    -- disable ligatures
+    harfbuzz_features = {
+      'calt=0', 'clig=0', 'liga=0'
+    },
   }),
-  color_scheme = "vague",
   use_fancy_tab_bar = false,
-  tab_bar_at_bottom = true,
-  native_macos_fullscreen_mode = true,
+  tab_bar_at_bottom = false,
+  window_decorations = "NONE",
   window_padding = {
     left = 0,
     right = 0,
